6da5c31a9afc0e267e74dc1351951534ad949cc4
quantumpotato <quantumpotato@gmail.com>
2017-02-26 20:46:04 -0500

    Isolated room tests for room.erl's clarity

--- room.erl
+++ room.erl
@@ -1,21 +1,5 @@
 -module(room).
 -compile(export_all).
--include_lib("eunit/include/eunit.hrl").
-
-join_test() ->
-  {ok, Room} = room:go(),
-  s:s(Room, {join, #{name => "Marsifrolg"}}),
-  {#{}, #{}, "Marsifrolg"} = s:s(Room, debug),
-  s:s(Room, {join, #{name => "Blandline"}}),
-  {Players, Tables, Challenger} = s:s(Room, debug),
-  [FirstPid] = maps:keys(Tables),
-  #{"Marsifrolg"  := #{status := playing, table_pid := FirstPid},
-   "Blandline"    := #{status := playing, table_pid := FirstPid}} = Players,
-  #{FirstPid      := #{seats  := #{x := "Marsifrolg", o := "Blandline"},
-                       status := unstarted}} = Tables,
-   null = Challenger,
-   {Players, Tables, Challenger}.
-
 
 join({Players, Tables, null}, #{name := ChallengerName}) ->
   {ok, {Players, Tables, ChallengerName}};
---
+++ room_test.erl
@@ -0,0 +1,15 @@
+-module(room_test).
+-include_lib("eunit/include/eunit.hrl").
+
+join_test() ->
+  {ok, Room} = room:go(),
+  s:s(Room, {join, #{name => "Marsifrolg"}}),
+  {#{}, #{}, "Marsifrolg"} = s:s(Room, debug),
+  s:s(Room, {join, #{name => "Blandline"}}),
+  {Players, Tables, Challenger} = s:s(Room, debug),
+  [FirstPid] = maps:keys(Tables),
+  #{"Marsifrolg"  := #{status := playing, table_pid := FirstPid},
+   "Blandline"    := #{status := playing, table_pid := FirstPid}} = Players,
+  #{FirstPid      := #{seats  := #{x := "Marsifrolg", o := "Blandline"},
+                       status := unstarted}} = Tables,
+   null = Challenger.
