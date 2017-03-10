42be8c0658880ab0631eb809f9c46701861a1cf3
quantumpotato <quantumpotato@gmail.com>
2017-03-10 00:09:59 -0500

    Clean up.

--- table_test.erl
+++ table_test.erl
@@ -10,8 +10,7 @@ x_takes_2_1_test() ->
   {ok, Table} = table:go(),
   s:s(Table, {place, take, x, {2, 1}}),
   #{board := [Row1 | _]} = s:s(Table, info),
-  % Row1.
   [{spawn, x, none}, {recent, x, none}, _, _, _]  = Row1.
 
-% first_turn_action_swap_test() ->
-%   false = test.
+first_turn_action_swap_test() ->
+  false = test.
