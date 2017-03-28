8310c6c4046e01d9d0eb788d164863f604df009d
quantumpotato <quantumpotato@gmail.com>
2017-03-27 20:47:27 -0400

    Regular play works

--- table_test.erl
+++ table_test.erl
@@ -23,3 +23,13 @@ out_of_order_test() ->
   s:s(Table, {place, take, o, {4, 5}}),
   #{actions := Actions} = s:s(Table, info),
   {_, x} = Actions.
+
+regular_play_test() ->
+  {ok, Table} = table:go(),
+  s:s(Table, {place, take, x, {2, 1}}),
+  s:s(Table, {place, take, o, {4, 5}}),
+  s:s(Table, {place, take, o, {4, 4}}),
+  s:s(Table, {place, take, x, {3, 1}}),
+  s:s(Table, {place, take, x, {3, 2}}),
+  #{actions := Actions} = s:s(Table, info),
+  {_, o} = Actions.
